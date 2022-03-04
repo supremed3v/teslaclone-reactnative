ab7f8ef0f570f885473d048000332993f6af9f7c
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2022-03-04 11:33:42 +0500

    final commit

---
+++ components/CarsList/index.js
@@ -0,0 +1,25 @@
+import { View, Text, FlatList, Dimensions } from 'react-native'
+import React from 'react'
+import CarItem from '../CarItem'
+import styles from './styles'
+
+import cars from "./cars"
+
+const CarsList = () => {
+    return (
+        <View style={styles.container}>
+            <FlatList 
+                data={cars}
+                renderItem={({item})=>
+                <CarItem car={item} />
+            }
+            showsVerticalScrollIndicator={false}
+            snapToAlignment={"start"}
+            decelerationRate={"normal"}
+            snapToInterval={Dimensions.get("window").height}
+            />
+        </View>
+    )
+}
+
+export default CarsList
---
+++ components/Header/index.js
@@ -0,0 +1,12 @@
+import { View, Image } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+export default function Header() {
+  return (
+    <View style={styles.container} >
+        <Image style={styles.logo} source={require("../../assets/images/logo.png")} />
+        <Image style={styles.menu} source={require("../../assets/images/menu.png")} />
+    </View>
+  )
+}
